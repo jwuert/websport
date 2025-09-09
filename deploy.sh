8fadde5844c023f95de6c5dc65d90a2d6a434496
Jan Würthner <jan.wuerthner@gmx.de>
2025-09-10 00:14:26 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-
-cp build/libs/websport.jar ../.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/demo/WEB-INF/lib/ -i
-cp /home/wuerthne/Proj/sport/build/libs/sport.jar ../.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/demo/WEB-INF/lib/ -i
